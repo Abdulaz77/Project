ee0ba3980e9a565e3eb5c7c02f6ccadbf859ca8c
Abdulaz77 <okabdulaz@gmail.com>
2025-01-09 14:33:31 +0500

    telegram bot
    there  are  some information about kia.

---
+++ kiabot/src/main/java/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(new MyBot("6527912497:AAEkVSwvFzM0rSI9vZ_i5r5-frFsUuXyTDE"));
+    }
+}
